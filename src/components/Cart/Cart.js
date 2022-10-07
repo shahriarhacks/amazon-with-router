282b37e1ca65b0306be4b9a4c65abbbf4b5056d1
Dev-Shifat <shahriar.ahmed.7878@gmail.com>
2022-10-07 15:57:07 +0600

    Router Install

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,7 +7,7 @@ const Cart = (props) => {
     let total = 0;
     let shipping = 0;
     let quantity = 0;
-    for(const product of cart){
+    for (const product of cart) {
         quantity = quantity + product.quantity;
         total = total + product.price * product.quantity;
         shipping = shipping + product.shipping;
@@ -20,8 +20,8 @@ const Cart = (props) => {
             <p>Selected Items: {quantity}</p>
             <p>Total price: ${total}</p>
             <p>Total Shipping: ${shipping}</p>
-            <p>Tax: {tax}</p>
-            <h5>Grand Total: {grandTotal.toFixed(2)}</h5>
+            <p>Tax: ${tax}</p>
+            <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
         </div>
     );
 };
